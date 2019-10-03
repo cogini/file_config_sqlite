d294cf2dc6e730d6404e4fe42dd8ef746f3b1bc1
Jake Morrison <jake@cogini.com>
2019-10-03 10:00:00 +0800

    Generate project

---
+++ lib/file_config_sqlite.ex
@@ -0,0 +1,18 @@
+defmodule FileConfigSqlite do
+  @moduledoc """
+  Documentation for FileConfigSqlite.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> FileConfigSqlite.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/file_config_sqlite_test.exs
@@ -0,0 +1,8 @@
+defmodule FileConfigSqliteTest do
+  use ExUnit.Case
+  doctest FileConfigSqlite
+
+  test "greets the world" do
+    assert FileConfigSqlite.hello() == :world
+  end
+end
