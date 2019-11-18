b6de2ea167d3f4f4e00554b4522a10204c2f3c01
Jake Morrison <jake@cogini.com>
2019-11-18 19:19:46 +0800

    Add hex metadata

--- mix.exs
+++ mix.exs
@@ -1,13 +1,32 @@
 defmodule FileConfigSqlite.MixProject do
   use Mix.Project
 
+  @github "https://github.com/cogini/file_config_sqlite"
+
   def project do
     [
       app: :file_config_sqlite,
       version: "0.1.0",
       elixir: "~> 1.8",
-      start_permanent: Mix.env() == :prod,
-      deps: deps()
+      elixirc_paths: elixirc_paths(Mix.env),
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
+      source_url: @github,
+      homepage_url: @github,
+      dialyzer: [
+        # plt_add_deps: :project,
+        # plt_add_apps: [:ssl, :mnesia, :compiler, :xmerl, :inets, :disk_log],
+        plt_add_deps: true,
+        # flags: ["-Werror_handling", "-Wrace_conditions"],
+        # flags: ["-Wunmatched_returns", :error_handling, :race_conditions, :underspecs],
+        # ignore_warnings: "dialyzer.ignore-warnings"
+      ],
+      deps: deps(),
+      docs: docs(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
     ]
   end
 
@@ -18,15 +37,46 @@ defmodule FileConfigSqlite.MixProject do
     ]
   end
 
+  # defp extra_applications(:test), do: []
+  # defp extra_applications(_),     do: []
+
+  # Specifies which paths to compile per environment
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:credo, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false},
       # {:esqlite, github: "mmzeeman/esqlite"},
       {:esqlite, "~> 0.4.0"},
+      {:ex_doc, "~> 0.19.2", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.12.0", only: [:dev, :test], runtime: false},
       {:nimble_csv, "~> 0.3"},
       {:sqlitex, "~> 1.7"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
+
+  defp description do
+    "SQLite file handler for file_config."
+  end
+
+  defp package do
+    [
+      maintainers: ["Jake Morrison"],
+      licenses: ["Mozilla Public License 2.0"],
+      links: %{"GitHub" => @github}
+    ]
+  end
+
+  defp docs do
+    [
+      source_url: @github,
+      extras: ["README.md"]
+    ]
+  end
+
 end
